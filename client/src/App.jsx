d0bbb4f0c6e1abb15b01a39da3db8bf3d71f7e60
Shariq Khan <khanshariq76543@gmail.com>
2024-02-11 17:19:44 +0530

    setup basic route pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { SignIn } from "./pages/signIn";
+import { SignUp } from "./pages/signUp";
+import { Dashboard } from "./pages/Dashboard";
+import { Projects } from "./pages/Projects";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
